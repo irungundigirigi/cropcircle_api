42cd8b3cfad172b9e74f9c60846d20a4ea3d0ae6
irungundigirigi <irungundigirigi@gmail.com>
2024-02-05 21:56:48 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const { Client } = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// PostgreSQL client setup
+const client = new Client({
+    connectionString: "postgres://default:J6gUW2seGlQR@ep-blue-haze-a4cb6wwh.us-east-1.postgres.vercel-storage.com:5432/verceldb", // Use environment variable to store the connection string
+    ssl: {
+        rejectUnauthorized: false, // Disable SSL certificate verification
+    },
+});
+
+client.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Error connecting to PostgreSQL database', err));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const corsOptions = {
+    origin: '*'
+};
+app.use(cors(corsOptions));
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
